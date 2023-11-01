5d05f1ce85d3842730ecdeca9eb422fd473ac9e2
ArmandoCivini <acivini@fi.uba.ar>
2023-10-31 22:15:25 -0300

    merge fix

--- src/master/src/partition.h
+++ src/master/src/partition.h
@@ -23,7 +23,6 @@ class Partition
     size_t n_intervals;
     std::vector<Interval> intervals;
     // Example partitions
-    std::array<std::array<int, 3>, 3> partitions = {{{0, 10, 1}, {0, 10, 1}, {0, 10, 1}}};
     int calc_amount_of_missing_partitions(int min_batches, std::vector<int>& partitions_per_interval);
     int calc_partitions_amount(std::vector<int>& partitions_per_interval);
     std::vector<std::vector<Interval>> cartesian_product(
