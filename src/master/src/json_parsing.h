fd6902d5b5bac1eaa0563a47de94357fbb602214
Facundo Mastricchio <facundomastricchio@gmail.com>
2023-11-03 22:01:35 -0300

    Move json parsing to single header file

--- src/master/src/json_parsing.h
+++ src/master/src/json_parsing.h
@@ -7,8 +7,18 @@
 
 using json = nlohmann::json;
 
-std::vector<Interval> jsonToIntervals(const json &json_params);
+std::vector<Interval> jsonToIntervals(const json &json_params) {
+    std::vector<Interval> intervals;
+    for (int i = 0; i < json_params.size(); i++)
+    {
+        intervals.emplace_back(json_params[i][0], json_params[i][1], json_params[i][2]);
+        if (json_params[i].size() > 3)
+        {
+            intervals[i].setPrecision(json_params[i][3]);
+        }
+    }
+    return intervals;
+}
 
-#include "json_parsing.cpp"
 
 #endif // JSON_PARSING_H
