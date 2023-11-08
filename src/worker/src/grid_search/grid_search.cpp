051e95cdf203edb694f10801d37d16d078f6b400
ArmandoCivini <acivini@fi.uba.ar>
2023-11-08 15:32:40 -0300

    removed unused functions in grid_search

--- src/worker/src/grid_search/grid_search.cpp
+++ src/worker/src/grid_search/grid_search.cpp
@@ -17,59 +17,9 @@ template <std::size_t Size> void GridSearch<Size>::search(std::function<float(st
     input = accumulator.get_input();
 }
 
-template <std::size_t Size> void GridSearch<Size>::initAccumulation(float res, std::array<float, Size> &current)
-{
-    max = res;
-    min = res;
-    total = res;
-    max_input = current;
-    min_input = current;
-    total_inputs = params_.get_total_iterations();
-}
-
-template <std::size_t Size> void GridSearch<Size>::accumulate(float res, std::array<float, Size> &current)
-{
-    if (res > max)
-    {
-        max = res;
-        max_input = current;
-    }
-    else if (res < min)
-    {
-        min = res;
-        min_input = current;
-    }
-    total += res;
-}
-
-template <std::size_t Size> float GridSearch<Size>::getMax()
-{
-    return max;
-}
-
-template <std::size_t Size> float GridSearch<Size>::getMin()
-{
-    return min;
-}
-
-template <std::size_t Size> float GridSearch<Size>::getTotal()
-{
-    return total;
-}
-
 template <std::size_t Size> int GridSearch<Size>::getTotalInputs()
 {
     return total_inputs;
-}
-
-template <std::size_t Size> std::array<float, Size> GridSearch<Size>::getMaxInput()
-{
-    return max_input;
-}
-
-template <std::size_t Size> std::array<float, Size> GridSearch<Size>::getMinInput()
-{
-    return min_input;
 }
 
 template <std::size_t Size> float GridSearch<Size>::getResult()
--- src/worker/src/grid_search/grid_search.h
+++ src/worker/src/grid_search/grid_search.h
@@ -13,28 +13,16 @@ template <std::size_t Size> class GridSearch
     {
     }
     void search(std::function<float(std::array<float, Size> &)> callback);
-    float getMax();
-    float getMin();
-    float getTotal();
     int getTotalInputs();
-    std::array<float, Size> getMaxInput();
-    std::array<float, Size> getMinInput();
     float getResult();
     std::array<float, Size> getInput();
 
   private:
     Params<Size> params_;
-    float max;
-    float min;
-    float total;
     float result;
     int total_inputs;
     std::string accum_type;
-    std::array<float, Size> max_input;
-    std::array<float, Size> min_input;
     std::array<float, Size> input;
-    void initAccumulation(float res, std::array<float, Size> &current);
-    void accumulate(float res, std::array<float, Size> &current);
 };
 
 #include "grid_search.cpp"
